9a5ef332db394b9cb23163c4dd3cb5cd10f0e939
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-09-07 12:09:08 -0300

    feat: handler exception in express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./router";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(router)
+app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    return response.json({
+      status: "error",
+      error: error.message,
+    });
+  }
+);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
